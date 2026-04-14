12aff37eaf41bd06a0dc2f41e9a9b933c8475824
alchier divino <alchierdivino009@gmail.com>
2026-04-14 22:34:54 +0800

    Updated main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import json
 import time
 import random
 
-ACCESS_TOKEN = "94r34Zx3xeDjt0Zu4qJz"
+ACCESS_TOKEN = "zf1ztnuspiiv8jv0u9r9"
 BROKER = "demo.thingsboard.io"  # or your server IP
 
 client = mqtt.Client()
