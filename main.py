419ede5df074f4fa92b1e0ccb1202d1bace97ab6
alchier divino <alchierdivino009@gmail.com>
2026-04-14 22:05:21 +0800

    Connect To ThingsBoard

---
+++ main.py
@@ -0,0 +1,23 @@
+import paho.mqtt.client as mqtt
+import json
+import time
+import random
+
+ACCESS_TOKEN = "YOUR_ACCESS_TOKEN"
+BROKER = "demo.thingsboard.io"  # or your server IP
+
+client = mqtt.Client()
+client.username_pw_set(ACCESS_TOKEN)
+
+client.connect(BROKER, 1883, 60)
+
+while True:
+    data = {
+        "temperature": random.randint(20, 35),
+        "humidity": random.randint(50, 80)
+    }
+
+    client.publish("v1/devices/me/telemetry", json.dumps(data))
+    print("Sent:", data)
+
+    time.sleep(5)
